0ba8a86ce50d244b9efa78f6bfbd363b3c0e468e
smyoo <smyoo@MAC-SMYOO.local>
2021-08-02 08:45:21 +0900

    프로젝트 구성
    - 초기 구성

---
+++ src/main/java/me/zepetto/zepettoland/ZepettolandApplication.java
@@ -0,0 +1,13 @@
+package me.zepetto.zepettoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZepettolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZepettolandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/zepetto/zepettoland/ZepettolandApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zepetto.zepettoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZepettolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
